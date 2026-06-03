ece0f42741f84855f0d1246a5dfd663fff788bca
Shreya Lambodari <shreyalambodari43@gmail.com>
2026-06-03 23:02:48 +0530

    Updated SecurityConfig

--- src/main/java/com/ecom_proj/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/ecom_proj/ecommerce/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
                         .permitAll()
 
                         .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
+
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
